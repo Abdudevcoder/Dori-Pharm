daa3370090ff6bd9d401b05c1aa252994bdc651c
Abdudevcoder <amuratov799@gmail.com>
2022-09-11 17:26:46 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/pictures/logo.png'
+
+
 
 function App() {
   return (
     <div className='html'>
+      {/* ----------------navbar---------------------- */}
       <div className='navbar'>
-        <div className='logo'>
-          
-        </div>
+        <img src={logo} alt='logo'></img>
+        <a className='navlink' href='products'>Продукця</a>
+        <a className='navlink' href='about'>О нас</a>
+        <a className='navlink' href='footer'>Контакты</a>
+        <a className='navlink' href='footer'>Связаться с нами</a>
       </div>
+      {/* ----------------navbar (end)---------------------- */}
 
+
+      {/* ----------------body---------------------- */}
+      <div className='body'>
+        <div className='products' id='products'>
+
+        </div>
+        <div className='about' id='about'>
+
+        </div>
+
+      </div>
+      {/* ----------------body (end)---------------------- */}
+
+
+
+
+      {/* ----------------footer---------------------- */}
+      <div className='footer' id="fotter">
+          <div className="footer__row">
+            <div className="footer__inner">
+                <div className="footer__left">
+                    <h1 className="company__name">Doripharm Future</h1>
+                    <p className="aboutUs">Мы являемся ведущими поставщиками из США в Центральную Азию.</p>
+                    
+                    <ul className="weOn">
+                      <li className="weOn__items"><a className="weOn__item" href="/">Inst</a></li>
+                      <li className="weOn__items"><a className="weOn__item" href="/">fb</a></li>
+                      <li className="weOn__items"><a className="weOn__item" href="/">tg</a></li>
+                    </ul>
+                </div>
+                <div className="footer__right">
+                    <div className="right__items">
+                      <h3>Адрес:</h3>
+                      <p>Республика Узбекистан, 100070, г. Ташкент, 
+                          Яккасарайский район, ул. Шота Руставели 12</p>
+                    </div>
+
+                    <div className="right__items">
+                      <h3>Номер телефона:</h3>
+                      <div className="tel">
+                        <a href="tel:+998 94 0718110">+998 94 0718110</a>
+                        <a href="tel:+998 94 0718110">+998 94 0718110</a>
+                      </div>
+                    </div>
+
+                    <div className="right__items">
+                      <h3>Почта:</h3>
+                      <a href="mailto:doripharmfuture@gmail.com">doripharmfuture@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      {/* ----------------footer (end)---------------------- */}
     </div>
   );
 }
